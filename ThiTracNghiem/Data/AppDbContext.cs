c477b39bc9d42a8cd9ab8450493a5b30e2810d4d
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2025-04-20 12:40:41 +0700

    fixdbDeThifixdbDeThi

--- ThiTracNghiem/Data/AppDbContext.cs
+++ ThiTracNghiem/Data/AppDbContext.cs
@@ -16,5 +16,22 @@ namespace ThiTracNghiem.Data
         public DbSet<DeThi> DeThis { get; set; }
         public DbSet<ChiTietDeThi> ChiTietDeThis { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ChiTietDeThi>()
+                .HasOne(ct => ct.DeThi)
+                .WithMany(d => d.ChiTietCauHoi)
+                .HasForeignKey(ct => ct.DeThiId)
+                .OnDelete(DeleteBehavior.Restrict); // tránh multiple cascade
+
+            modelBuilder.Entity<ChiTietDeThi>()
+                .HasOne(ct => ct.CauHoi)
+                .WithMany()
+                .HasForeignKey(ct => ct.CauHoiId)
+                .OnDelete(DeleteBehavior.Cascade); // vẫn cho phép xóa câu hỏi kéo theo
+        }
+
     }
 }
